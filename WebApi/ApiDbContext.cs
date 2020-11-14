8cfba79dfa677ed27921e9fa683e123e58800836
Yuvaraj Kesavan <yuvaraj0615@gmail.com>
2020-11-14 05:37:58 +0000

    Added Files

---
+++ WebApi/ApiDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi
+{
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) :
+            base(options)
+        {
+        }
+
+        public DbSet<Blog> Blogs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            new BlogMap(builder.Entity<Blog>());
+        }
+    }
+
+    public class BlogMap
+    {
+        public BlogMap(EntityTypeBuilder<Blog> entityBuilder)
+        {
+            entityBuilder.HasKey(x => x.Id);
+            entityBuilder.ToTable("blog");
+            entityBuilder.Property(x => x.Id).HasColumnName("id");
+            entityBuilder.Property(x => x.Title).HasColumnName("title");
+            entityBuilder.Property(x => x.Description).HasColumnName("description");
+        }
+    }
+}
---
+++ WebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace WebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IHttpClientFactory _clientFactory;
+        public IEnumerable<WeatherForecast> Branches { get; private set; }
+
+        public IndexModel(ILogger<IndexModel> logger, IHttpClientFactory clientFactory)
+        {
+            _logger = logger;
+            _clientFactory = clientFactory;
+        }
+
+        public async Task OnGet()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get,
+            "http://webapi/weatherforecast");
+
+            var client = _clientFactory.CreateClient();
+
+            var response = await client.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                using var responseStream = await response.Content.ReadAsStreamAsync();
+                Branches = await JsonSerializer.DeserializeAsync
+                    <IEnumerable<WeatherForecast>>(responseStream);
+            }
+            else
+            {
+                //GetBranchesError = true;
+                Branches = Array.Empty<WeatherForecast>();
+            }
+        }
+    }
+}
